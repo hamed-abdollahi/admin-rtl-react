50beef32f33cd687ed4f8012667e5c84f8616e6a
Developer <h@tt.com>
2023-10-15 17:39:55 +0330

    add serviceWorker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { Provider } from 'react-redux'
 import store from './store'
 
@@ -17,5 +17,6 @@ createRoot(document.getElementById('root')).render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.register();
+serviceWorkerRegistration.register();
+
 reportWebVitals()
